026e90a802f13b314ccbade1eb6965f3907de79f
MOHIT KUMAR <gpc.mohitkumar@gmail.com>
2026-09-23 11:36:24 +0530

    Q13

---
+++ 13_To_Create_a_program_to_calculate_product_of_two_floating_point_numbers_in_c.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+ float a,b,product;
+ printf("\nEnter First Floating Point Value = ");
+ scanf("%f",&a);
+ printf("\nEnter Second Floating Point Value = ");
+ scanf("%f",&b);
+ product=a*b;
+ printf("\nThe Product of two numbers is %f",product);
+ return 0;
+
+}
