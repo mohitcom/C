c2b644df07104e454085119b3adefbf565845f87
MOHIT KUMAR <gpc.mohitkumar@gmail.com>
2026-09-23 11:42:42 +0530

    Q16

---
+++ 18_To_Create_a_program_to_convert_fahrenheit_to_celsius_in_c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  float c,f;
+  printf("Enter The Fahrenheit = ");
+  scanf("%f",&f);
+  c=(f-32)*5/9;
+  printf("The fahrenheit to celsius is %f",c);
+  return 0;
+  
+}
