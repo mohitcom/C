86369ad9a4f848a43e1b5362cc7db0f0a0e2d37f
MOHIT KUMAR <gpc.mohitkumar@gmail.com>
2026-09-23 12:01:53 +0530

    Q33

---
+++ 33_to_write_a_function_that_calculate_the_factorial_of_a_given_number_in_c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+unsigned long long factorial(int n) {
+    unsigned long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    printf("Factorial of %d is %llu\n", num, factorial(num));
+    return 0;
+}
